e8c1c1f53f5d9c60db5f54cc818ccfa591a36bb6
maopengchao <maopengchao@xinhuamm.net>
2026-07-12 21:36:07 +0800

    feat: add SEO optimization (meta tags, robots.txt, sitemap.xml, JSON-LD)

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import "./index.css";
 import "./i18n";
 import App from "./App.tsx";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")!).render(
   <>
     <App />
     <Analytics />
+    <SpeedInsights />
   </>,
 );
