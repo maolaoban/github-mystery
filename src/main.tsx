efd9c2b8ef0d67752490d708ac597f84d2b69c45
maopengchao <maopengchao@xinhuamm.net>
2026-07-12 21:24:40 +0800

    chore: update vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./i18n";
 import App from "./App.tsx";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <>
